0f6198157d4c40b12f17d188c89b8f829c33f92d
kotevatankica <koteva.tankica@gmail.com>
2022-11-07 15:50:06 +0100

    first commit

---
+++ src/components/File.vue
@@ -0,0 +1,43 @@
+<script >
+import axios from "axios";
+import FileList from './FileList.vue';
+
+export default {
+    components: {
+        FileList,
+    },
+    data() {
+
+        return {
+            files: '',
+        }
+    },
+
+    mounted() {
+
+        const username = 'adamantus';
+        const password = 'lemon7';
+        axios.get('https://php72.afk.mk/young_blood/tankica/file/index.php', {
+            headers: {
+                authorization: "Basic " + btoa(username + ":" + password)
+            }
+        }).then((resp) => {
+            this.files = resp.data;
+            console.log(resp.data)
+        });
+
+
+
+    }
+}
+</script>
+
+<template>
+
+    <FileList v-for="item in files" :item="item" />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FileList.vue
@@ -0,0 +1,28 @@
+<script >
+
+export default {
+    name: 'file-sub-list',
+    props: ['item'],
+
+}
+</script>
+
+<template>
+    <ul>
+
+        <li>
+            <p>{{ item.name }}</p>
+            <p>{{ item.type }}</p>
+
+            <file-sub-list v-if="typeof item.data.subs != 'undefined'" v-for="sub in item.data.subs" :item="sub">
+                <p>{{ item.name }}</p>
+                <p>{{ item.type }}</p>
+                git init
+            </file-sub-list>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
