b24887cea224fd8930dc7cbb480fae705361cab5
kotevatankica <koteva.tankica@gmail.com>
2022-11-09 15:54:10 +0100

    some changes

--- src/components/File.vue
+++ src/components/File.vue
@@ -1,43 +1,60 @@
 <script >
 import axios from "axios";
 import FileList from './FileList.vue';
+import { ref } from "vue";
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
 
 export default {
     components: {
         FileList,
+        Datepicker
     },
-    data() {
+    setup() {
+        const files = ref([]);
+        const starting_date = ref();
+        const ending_date = ref();
+
 
         return {
-            files: '',
+            files: files, starting_date: starting_date, ending_date: ending_date
         }
     },
 
     mounted() {
-
         const username = 'adamantus';
         const password = 'lemon7';
-        axios.get('https://php72.afk.mk/young_blood/tankica/file/index.php', {
+        axios.get('https://php72.afk.mk/images/api.php', {
             headers: {
                 authorization: "Basic " + btoa(username + ":" + password)
             }
         }).then((resp) => {
             this.files = resp.data;
-            console.log(resp.data)
         });
-
-
-
     }
 }
 </script>
 
 <template>
 
+    <div>
+        <div>
+            <span>Older than:</span>
+            <Datepicker v-model="starting_date" />
+        </div>
+        <br>
+        <div>
+            <span >Younger than: </span>
+            <Datepicker v-model="ending_date" />
+        </div>
+    </div>
+
     <FileList v-for="item in files" :item="item" />
 
 </template>
 
 <style scoped>
-
+span {
+    color: black;
+}
 </style>
--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -1,28 +1,88 @@
 <script >
-
+import { ref } from 'vue';
 export default {
     name: 'file-sub-list',
     props: ['item'],
 
+    setup(props) {
+        const files = ref(props);
+        const show = ref(false);
+        return {
+            files: files,
+            show: show,
+        }
+    },
+
+    methods: {
+
+    }
 }
 </script>
 
 <template>
-    <ul>
+    <template v-for="item in files" :item="item">
+        <template v-if="item.type == 'dir'">
+            <div class="mainName">
+                <a href="#" @click="show = !show">
+                    <img src="../assets/icoooon.svg" alt="" class="dir-img"> </a>
+                <div>
+                    <div class="name">{{ item.name }} </div>
+                    <div class="files"> {{ item.data.count_dir_files }} files</div>
+                    <div> Size: <span> {{ item.data.get_directory_size }}</span></div>
+                </div>
 
-        <li>
-            <p>{{ item.name }}</p>
-            <p>{{ item.type }}</p>
+            </div>
+            <div v-if="item.subs && show">
+                <file-sub-list v-if="typeof item.subs != 'undefined'" v-for="sub in item.subs" :item="sub">
+                </file-sub-list>
+            </div>
 
-            <file-sub-list v-if="typeof item.data.subs != 'undefined'" v-for="sub in item.data.subs" :item="sub">
-                <p>{{ item.name }}</p>
-                <p>{{ item.type }}</p>
-                git init
-            </file-sub-list>
-        </li>
-    </ul>
+        </template>
+
+        <template v-else>
+
+            <div class="mainName">
+                <img src="../assets/file.svg" alt="" class="dir-img">
+                <div>
+                    <div class="name bold">{{ item.name }}</div>
+                    <div> Size: <span> {{ item.data.file_size }}</span></div>
+                    <div><span>Last modified:</span>{{ item.data.last_modified }}</div>
+                </div>
+            </div>
+        </template>
+
+
+    </template>
 </template>
 
 <style scoped>
+.mainName {
+    clear: both;
+    min-height: 70px;
+    border-top: solid 1px #ECE9E9;
+    display: flex;
+    flex-flow: row wrap;
+    margin-left: 15px;
+    margin-top: 9px;
+    color: black;
+}
 
+.name {
+    color: rgb(62, 62, 142);
+    font-weight: bolder;
+}
+
+.files {
+    margin-right: 50px;
+}
+
+.bold {
+    font-weight: 900;
+}
+
+.dir-img {
+    width: 50px;
+    height: 50px;
+    margin-right: 10px;
+}
 </style>
